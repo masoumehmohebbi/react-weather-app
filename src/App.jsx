ff8a1f9ac616baadf86749d558c243f0c71e3418
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-09-14 16:47:59 +0330

    Added Forecast CMP App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import ForeCast from "./Components/ForeCast";
+import Header from "./Components/Header";
+
 function App() {
-  return <div className="text-green-500">welcome</div>;
+  return (
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-cyan-600 bg-gradient-to-br h-fit shadow-xl shadow-gray-400">
+      <Header />
+      <ForeCast title={"Hourly forcast"} />
+      <ForeCast title={"daily forcast"} />
+    </div>
+  );
 }
 
 export default App;

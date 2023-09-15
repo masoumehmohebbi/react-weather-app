4c7059646017c500121f37d69b79139fcff4baf0
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-09-15 20:06:48 +0330

    Added Forecast.jsx

--- src/Components/Forecast.jsx
+++ src/Components/Forecast.jsx
@@ -1,15 +1,32 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import axios from "axios";
+import { useEffect } from "react";
 import { WiDayCloudy } from "react-icons/wi";
-function ForeCast({ title }) {
+function ForeCast({ title, weather }) {
+  console.log(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${weather.coord.lat}&lon=${weather.coord.lon}&appid=70e424f655b9c47fae9ea4df400c4cca`
+  );
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/onecall?lat=${weather.coord.lat}&lon=${weather.coord.lon}&appid=70e424f655b9c47fae9ea4df400c4cca`
+        )
+        .then((res) => console.log(res.data))
+        .catch((err) => console.log(err));
+    };
+    fetchData;
+  }, []);
   return (
     <section className="mt-8 text-white">
       <h1 className="uppercase font-bold">{title}</h1>
       <hr className="my-2" />
       <div className="flex space-x-8">
-        <p className="flex flex-col items-center">
+        <div className="flex flex-col items-center">
           <h5>02:00 PM</h5>
           <WiDayCloudy className="my-4" size={35} />
           <h5>19°</h5>
-        </p>
+        </div>
       </div>
     </section>
   );

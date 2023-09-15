4977258b2b56345bee23dde092ae1f695fa29224
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-09-15 20:04:03 +0330

    fetchedData App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,54 @@
-import Details from "./Components/Details";
+import { useEffect, useState } from "react";
+import Details, { Detail, Temperature } from "./Components/Details";
 import ForeCast from "./Components/ForeCast";
-import Header from "./Components/Header";
+import Header, { SearchBar, TimeAndLocation } from "./Components/Header";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
+import NavBar from "./Components/NavBar";
 
 function App() {
+  const [query, setQuery] = useState({ q: "rasht" });
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${query.q}&appid=70e424f655b9c47fae9ea4df400c4cca`
+        )
+
+        .then((res) => {
+          toast(
+            `Successfully fetched weather for ${res.data.name}, ${res.data.sys.country}.`
+          );
+          return setWeather(res.data);
+        })
+        .catch((err) => console.log(err));
+    };
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query]);
+
+  if (weather === null) {
+    return "Loading weather...";
+  }
   return (
-    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-cyan-600 bg-gradient-to-br h-fit shadow-xl shadow-gray-400">
-      <Header />
-      <Details />
-      <ForeCast title={"Hourly forcast"} />
-      <ForeCast title={"daily forcast"} />
-    </div>
+    <section>
+      <NavBar />
+      <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-cyan-600 bg-gradient-to-br h-fit shadow-xl shadow-gray-400">
+        <Toaster />
+        <Header>
+          <SearchBar setQuery={setQuery} />
+          <TimeAndLocation weather={weather} />
+        </Header>
+        <Details>
+          <Detail weather={weather} />
+          <Temperature weather={weather} />
+        </Details>
+        <ForeCast title={"Hourly forcast"} weather={weather} />
+        <ForeCast title={"daily forcast"} weather={weather} />
+      </div>
+    </section>
   );
 }
 

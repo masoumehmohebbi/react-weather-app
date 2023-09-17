5af601a29cffe2479c08bbefc26588b1db9aa79b
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-09-17 20:56:43 +0330

    Added toast.success App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Header, {
 import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
 import ForeCast from "./Components/Forecast";
+import Loader from "./Components/Loader";
 
 function App() {
   const [query, setQuery] = useState({ q: "rasht" });
@@ -20,7 +21,7 @@ function App() {
           `https://api.openweathermap.org/data/2.5/weather?q=${query.q}&appid=70e424f655b9c47fae9ea4df400c4cca`
         )
         .then((res) => {
-          toast(
+          toast.success(
             `Successfully fetched weather for ${res.data.name}, ${res.data.sys.country}.`
           );
           return setWeather(res.data);
@@ -32,13 +33,17 @@ function App() {
   }, [query]);
 
   if (weather === null) {
-    return "Loading weather...";
+    return (
+      <div className="pt-64">
+        <Loader />
+      </div>
+    );
   }
 
   return (
     <section className="flex justify-center bg-[#faf5ff]">
-      <div className="mx-auto min-w-max w-3/5 my-4 py-5 px-20 bg-contain bg-white text-slate-800 border border-[#e9d5ff] rounded-md h-fit shadow-xl">
-        <Toaster />
+      <div className="mx-auto w-11/12 px-5 lg:w-3/5 my-4 py-5 lg:px-20 bg-contain bg-white text-slate-800 border border-[#e9d5ff] rounded-md h-fit shadow-xl">
+        <Toaster position="top-right" />
         <Header>
           <NavBar setQuery={setQuery} />
           <SearchBar setQuery={setQuery} />

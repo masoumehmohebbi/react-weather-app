5ca867754aa8877d26244d3f71771b61b4ae8520
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-09-18 21:36:44 +0330

    Added isOpenNav state App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
 import ForeCast from "./Components/Forecast";
 import Loader from "./Components/Loader";
+import SideNav from "./Components/SideNav";
 
 const apiKey = import.meta.env.VITE_API_KEY;
 const apiUrl = import.meta.env.VITE_API_URL;
@@ -16,6 +17,7 @@ const apiUrl = import.meta.env.VITE_API_URL;
 function App() {
   const [query, setQuery] = useState({ q: "tehran" });
   const [weather, setWeather] = useState(null);
+  const [isOpenNav, setIsOpenNav] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -53,7 +55,11 @@ function App() {
 
   return (
     <section className="flex justify-center bg-[#faf5ff]">
-      <div className="mx-auto w-11/12 px-5 lg:w-3/5 my-4 py-5 lg:px-20 bg-contain bg-white text-slate-800 border border-[#e9d5ff] rounded-md h-fit shadow-xl">
+      <div
+        className={`
+      ${isOpenNav && "lg:rounded-tr-none"}
+      w-11/12 px-5 lg:w-3/5 my-4 py-5 lg:px-20 bg-contain bg-white text-slate-800 border border-[#e9d5ff] rounded-md h-fit shadow-xl`}
+      >
         <Toaster position="top-right" />
         <Header>
           <NavBar setQuery={setQuery} />
@@ -66,6 +72,7 @@ function App() {
         </Details>
         <ForeCast title={"Hourly forcast"} query={query} />
       </div>
+      <SideNav isOpenNav={isOpenNav} setIsOpenNav={setIsOpenNav} />
     </section>
   );
 }

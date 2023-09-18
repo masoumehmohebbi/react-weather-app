c32ad6c98507325a438951d1047bc14bf175d347
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-09-18 23:27:45 +0330

    Commented Loader-code Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,55 +1,52 @@
-import { useEffect, useState } from "react";
 import Details, { Detail, Temperature } from "../Components/Details";
 import Header, {
   NavBar,
   SearchBar,
   TimeAndLocation,
 } from "../Components/Header";
-import axios from "axios";
-import toast, { Toaster } from "react-hot-toast";
 import ForeCast from "../Components/Forecast";
-import Loader from "../Components/Loader";
+import { Toaster } from "react-hot-toast";
+// import Loader from "../Components/Loader";
 
-const apiKey = import.meta.env.VITE_API_KEY;
-const apiUrl = import.meta.env.VITE_API_URL;
+// const apiKey = import.meta.env.VITE_API_KEY;
+// const apiUrl = import.meta.env.VITE_API_URL;
 
-function Home() {
-  const [query, setQuery] = useState({ q: "tehran" });
-  const [weather, setWeather] = useState(null);
+function Home({ weather, setQuery, query }) {
+  // const [query, setQuery] = useState({ q: "tehran" });
 
-  useEffect(() => {
-    const fetchData = async () => {
-      await axios
-        .get(`${apiUrl}/weather?q=${query.q}&appid=${apiKey}`)
-        .then((res) => {
-          toast.success((t) => (
-            <div className="relative grid grid-cols-4 gap-2">
-              <span className="col-span-3">
-                {`Successfully fetched weather for ${res.data.name},${res.data.sys.country}.`}
-              </span>
-              <button
-                className="col-span-1 text-red-600 text-xl absolute right-0 top-0 flex items-center justify-center"
-                onClick={() => toast.dismiss(t.id)}
-              >
-                X
-              </button>
-            </div>
-          ));
-          return setWeather(res.data);
-        })
-        .catch((err) => console.log(err));
-    };
-    fetchData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [query]);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     await axios
+  //       .get(`${apiUrl}/weather?q=${query.q}&appid=${apiKey}`)
+  //       .then((res) => {
+  //         toast.success((t) => (
+  //           <div className="relative grid grid-cols-4 gap-2">
+  //             <span className="col-span-3">
+  //               {`Successfully fetched weather for ${res.data.name},${res.data.sys.country}.`}
+  //             </span>
+  //             <button
+  //               className="col-span-1 text-red-600 text-xl absolute right-0 top-0 flex items-center justify-center"
+  //               onClick={() => toast.dismiss(t.id)}
+  //             >
+  //               X
+  //             </button>
+  //           </div>
+  //         ));
+  //         return setWeather(res.data);
+  //       })
+  //       .catch((err) => console.log(err));
+  //   };
+  //   fetchData();
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [query]);
 
-  if (weather === null) {
-    return (
-      <div className="pt-64">
-        <Loader />
-      </div>
-    );
-  }
+  // if (weather === null) {
+  //   return (
+  //     <div className="pt-64">
+  //       <Loader />
+  //     </div>
+  //   );
+  // }
 
   return (
     <>

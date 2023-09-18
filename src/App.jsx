dcfccf548a6effe430188704b38cf007058126bf
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-09-18 22:14:03 +0330

    Added Layout CMP App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Layout from "./Components/Layout";
+import { useState } from "react";
 
 function App() {
-  return <Home />;
+  const [isOpenNav, setIsOpenNav] = useState(true);
+
+  return (
+    <Routes>
+      <Route
+        element={<Layout isOpenNav={isOpenNav} setIsOpenNav={setIsOpenNav} />}
+      >
+        <Route path="/" exact element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;

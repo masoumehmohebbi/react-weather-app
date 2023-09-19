ed7258982306545c50673fa41a1631b11069fe5c
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-09-19 13:37:04 +0330

    Rendered useWeather-Hook App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Layout from "./Components/Layout";
-import { useEffect, useState } from "react";
 import Loader from "./Components/Loader";
-import toast from "react-hot-toast";
-import axios from "axios";
+import useWeather from "./hooks/useWeather";
+import { useState } from "react";
+
 const apiKey = import.meta.env.VITE_API_KEY;
 const apiUrl = import.meta.env.VITE_API_URL;
 
 function App() {
   const [isOpenNav, setIsOpenNav] = useState(true);
-  const [weather, setWeather] = useState(null);
-  const [query, setQuery] = useState({ q: "tehran" });
 
-  useEffect(() => {
-    const fetchData = async () => {
-      await axios
-        .get(`${apiUrl}/weather?q=${query.q}&appid=${apiKey}`)
-        .then((res) => {
-          toast.success((t) => (
-            <div className="relative grid grid-cols-4 gap-2">
-              <span className="col-span-3">
-                {`Successfully fetched weather for ${res.data.name},${res.data.sys.country}.`}
-              </span>
-              <button
-                className="col-span-1 text-red-600 text-xl absolute right-0 top-0 flex items-center justify-center"
-                onClick={() => toast.dismiss(t.id)}
-              >
-                X
-              </button>
-            </div>
-          ));
-          return setWeather(res.data);
-        })
-        .catch((err) => console.log(err));
-    };
-    fetchData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [query]);
+  const { query, setQuery, weather } = useWeather(apiUrl, apiKey);
 
   if (weather === null) {
     return (
@@ -47,6 +21,7 @@ function App() {
       </div>
     );
   }
+
   return (
     <Routes>
       <Route

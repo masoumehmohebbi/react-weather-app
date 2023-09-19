2d638b13a9f2d8e3ddda245def40d045e48cfc7d
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-09-19 10:21:25 +0330

    Rendered MobileNav

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,15 +1,17 @@
 import { Outlet } from "react-router-dom";
 import SideNav from "./SideNav";
+import MobileNav from "./MobileNav";
 
 function Layout({ isOpenNav, setIsOpenNav }) {
   return (
     <section className="flex justify-center">
       <div
         className={`
-        ${isOpenNav && "lg:rounded-tr-none"}
-        w-11/12 px-5 lg:w-3/5 my-4 py-5 lg:px-20 bg-contain bg-white text-slate-800 border border-[#e9d5ff] rounded-md h-fit shadow-xl`}
+        ${isOpenNav && "sm:rounded-tr-none"}
+        w-11/12 px-5 lg:w-3/5 my-4 mb-11 py-5 lg:px-20 bg-contain bg-white text-slate-800 border border-[#e9d5ff] rounded-md h-fit shadow-xl`}
       >
         <Outlet />
+        <MobileNav />
       </div>
       <SideNav isOpenNav={isOpenNav} setIsOpenNav={setIsOpenNav} />
     </section>
